2ed95929132d15f9058fe349adc20386774b0e63
Dhruv Pandey <dhruvrpandey01@gmail.com>
2026-03-24 20:55:01 +0530

    chore: initialize project setup

---
+++ core-utils-service/src/main/java/com/dhruv/coreutils/CoreUtilsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dhruv.coreutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreUtilsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreUtilsServiceApplication.class, args);
+	}
+
+}
---
+++ core-utils-service/src/test/java/com/dhruv/coreutils/CoreUtilsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhruv.coreutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreUtilsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
